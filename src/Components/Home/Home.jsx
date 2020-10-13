a154121e151aa19cec77f1ef2eda621de92731b4
Javier Hardman <0015@citad.local>
2020-10-13 10:03:25 -0600

    removed yarn

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,16 +4,17 @@ export default class Home extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {  };
+        this.state = {};
     }
 
-    componentDidMount() {
-    }
+    componentDidMount() {}
 
     render() {
         return (
             <div id="login_container" className="container-fluid">
-                <div className="row"><div className="col-12">Hello, big things are coming...</div></div>
+                <div className="row">
+                    <div className="col-12">Hello, still working on it.</div>
+                </div>
             </div>
         );
     }
