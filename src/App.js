bb70bf5f0ceca0177616024789bd1ae85b4afbdb
Javier Hardman <0015@citad.local>
2020-10-23 14:47:05 -0600

    project reboot

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from "react";
-import "./App.css";
-import routes from "./routes";
-import { withRouter } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.PureComponent {
-    constructor(props) {
-        super(props);
-
-        this.state = {};
-    }
-
-    render() {
-        return <div className="App">{routes}</div>;
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default withRouter(App);
+export default App;
