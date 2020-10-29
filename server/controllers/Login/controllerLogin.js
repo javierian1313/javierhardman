c455aed4499420d0c6da087cf832012c1ac2d5db
Javier Hardman <0015@citad.local>
2020-10-29 17:32:38 -0600

    eod

--- server/controllers/Login/controllerLogin.js
+++ server/controllers/Login/controllerLogin.js
@@ -2,29 +2,29 @@ const db = require("../../db");
 const bcrypt = require("bcrypt");
 
 module.exports = {
-    order1: function (req, res) {
-        const { username, password } = req.body;
+    // order1: function (req, res) {
+    //     const { username, password } = req.body;
 
-        db.query(`SELECT * FROM user WHERE username = '${username}' LIMIT 1`, function (error, results, fields) {
-            if (!results[0]) {
-                return res.status(200).send({ error: "username" });
-            } else {
-                const prevent_login = results[0].prevent_login;
-                const password_valid = bcrypt.compareSync(password, results[0].password);
+    //     db.query(`SELECT * FROM user WHERE username = '${username}' LIMIT 1`, function (error, results, fields) {
+    //         if (!results[0]) {
+    //             return res.status(200).send({ error: "username" });
+    //         } else {
+    //             const prevent_login = results[0].prevent_login;
+    //             const password_valid = bcrypt.compareSync(password, results[0].password);
 
-                if (prevent_login === 1) {
-                    return res.status(200).send({ error: "locked" });
-                } else {
-                    if (!password_valid) return res.status(200).send({ error: "password" });
-                    else res.status(200).send({ auth: 1 });
-                }
-            }
+    //             if (prevent_login === 1) {
+    //                 return res.status(200).send({ error: "locked" });
+    //             } else {
+    //                 if (!password_valid) return res.status(200).send({ error: "password" });
+    //                 else res.status(200).send({ auth: 1 });
+    //             }
+    //         }
 
-            if (error) throw error;
+    //         if (error) throw error;
 
-            res.end(JSON.stringify(results));
-        });
-    },
+    //         res.end(JSON.stringify(results));
+    //     });
+    // },
     order2: function (req, res) {
         const { session_id } = req.body;
 
--- server/routes/routerExample.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controllerExample = require("../controllers/controllerExample");
-
-router.get("/dbTest", controllerExample.order1);
-
-module.exports = router;
